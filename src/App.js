1354c91a53d6f4ee1024a474fbd1c554d0b76895
iceisaree <iceisaree1@gmail.com>
2020-11-04 10:09:56 +0700

    can play but human always play first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Button, Box } from "@material-ui/core";
 import "./App.css";
 
 function App() {
   const [human, setHuman] = useState("");
   const [ai, setAi] = useState("");
-  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [board, setBoard] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8]);
   const [channel0, setChannel0] = useState("");
   const [channel1, setChannel1] = useState("");
   const [channel2, setChannel2] = useState("");
@@ -15,18 +15,174 @@ function App() {
   const [channel6, setChannel6] = useState("");
   const [channel7, setChannel7] = useState("");
   const [channel8, setChannel8] = useState("");
+  const [iter, setIter] = useState(0);
+  const [round, setRound] = useState(0);
   console.log("human", human);
   console.log("ai", ai);
   console.log("board", board);
   console.log("board[0]", board[0]);
 
-  const handleClickTable = (index) => {
-    let newboard = board;
-    newboard[index] = human;
-    setBoard(newboard);
-    console.log("board", board);
-    return;
+  const handleClickTable = async (index, player) => {
+    if (board[index] !== "X" && board[index] !== "O") {
+      setRound(round + 2);
+      console.log("round", round);
+      let newboard = board;
+      newboard[index] = human;
+      setBoard(newboard);
+      console.log("board", board);
+      if (winning(board, player)) {
+        setTimeout(function () {
+          alert("YOU WIN");
+          reset();
+        }, 500);
+        return;
+      } else if (round > 7) {
+        setTimeout(function () {
+          alert("TIE");
+          reset();
+        }, 500);
+        return;
+      } else {
+        // setRound(round + 1);
+        // console.log('roundAi', round)
+        let aiIndex = minimax(board, ai).index;
+        let newboard = board;
+        newboard[aiIndex] = ai;
+        setBoard(newboard);
+        if (aiIndex === 0) {
+          setChannel0(ai);
+        } else if (aiIndex === 1) {
+          setChannel1(ai);
+        } else if (aiIndex === 2) {
+          setChannel2(ai);
+        } else if (aiIndex === 3) {
+          setChannel3(ai);
+        } else if (aiIndex === 4) {
+          setChannel4(ai);
+        } else if (aiIndex === 5) {
+          setChannel5(ai);
+        } else if (aiIndex === 6) {
+          setChannel6(ai);
+        } else if (aiIndex === 7) {
+          setChannel7(ai);
+        } else {
+          setChannel8(ai);
+        }
+        console.log("board", board);
+        if (winning(board, ai)) {
+          setTimeout(function () {
+            alert("YOU LOSE");
+            reset();
+          }, 500);
+          return;
+        }
+        // else if (round === 0) {
+        //   setTimeout(function () {
+        //     alert("tie");
+        //     reset();
+        //   }, 500);
+        //   return;
+        // }
+        else if (round > 7) {
+          setTimeout(function () {
+            alert("tie");
+            reset();
+          }, 500);
+          return;
+        }
+      }
+    }
   };
+
+  function winning(board, player) {
+    if (
+      (board[0] === player && board[1] === player && board[2] === player) ||
+      (board[3] === player && board[4] === player && board[5] === player) ||
+      (board[6] === player && board[7] === player && board[8] === player) ||
+      (board[0] === player && board[3] === player && board[6] === player) ||
+      (board[1] === player && board[4] === player && board[7] === player) ||
+      (board[2] === player && board[5] === player && board[8] === player) ||
+      (board[0] === player && board[4] === player && board[8] === player) ||
+      (board[2] === player && board[4] === player && board[6] === player)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function minimax(reboard, player) {
+    setIter(iter + 1);
+    let array = avail(reboard);
+    if (winning(reboard, human)) {
+      return {
+        score: -10,
+      };
+    } else if (winning(reboard, ai)) {
+      return {
+        score: 10,
+      };
+    } else if (array.length === 0) {
+      return {
+        score: 0,
+      };
+    }
+
+    let moves = [];
+    for (let i = 0; i < array.length; i++) {
+      let move = {};
+
+      move.index = reboard[array[i]];
+      reboard[array[i]] = player;
+
+      if (player === ai) {
+        let g1 = minimax(reboard, human);
+        move.score = g1.score;
+      } else {
+        let g2 = minimax(reboard, ai);
+        move.score = g2.score;
+      }
+      reboard[array[i]] = move.index;
+      moves.push(move);
+    }
+
+    let bestMove;
+    if (player === ai) {
+      let bestScore = -10000;
+      for (let i = 0; i < moves.length; i++) {
+        if (moves[i].score > bestScore) {
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    } else {
+      let bestScore = 10000;
+      for (let i = 0; i < moves.length; i++) {
+        if (moves[i].score < bestScore) {
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    }
+    return moves[bestMove];
+  }
+  function avail(reboard) {
+    return reboard.filter((s) => s !== "X" && s !== "O");
+  }
+
+  function reset() {
+    setRound(0);
+    setBoard([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+    setChannel0("");
+    setChannel1("");
+    setChannel2("");
+    setChannel3("");
+    setChannel4("");
+    setChannel5("");
+    setChannel6("");
+    setChannel7("");
+    setChannel8("");
+  }
 
   return (
     <div className="App">
@@ -64,7 +220,7 @@ function App() {
                 my="auto"
                 className="box"
                 onClick={() => {
-                  handleClickTable(0);
+                  handleClickTable(0, human);
                   setChannel0(human);
                 }}
               >
@@ -74,7 +230,7 @@ function App() {
                 my="auto"
                 className="box"
                 onClick={() => {
-                  handleClickTable(1);
+                  handleClickTable(1, human);
                   setChannel1(human);
                 }}
               >
@@ -84,7 +240,7 @@ function App() {
                 my="auto"
                 className="box"
                 onClick={() => {
-                  handleClickTable(2);
+                  handleClickTable(2, human);
                   setChannel2(human);
                 }}
               >
@@ -96,7 +252,7 @@ function App() {
                 my="auto"
                 className="box"
                 onClick={() => {
-                  handleClickTable(3);
+                  handleClickTable(3, human);
                   setChannel3(human);
                 }}
               >
@@ -106,7 +262,7 @@ function App() {
                 my="auto"
                 className="box"
                 onClick={() => {
-                  handleClickTable(4);
+                  handleClickTable(4, human);
                   setChannel4(human);
                 }}
               >
@@ -116,7 +272,7 @@ function App() {
                 my="auto"
                 className="box"
                 onClick={() => {
-                  handleClickTable(5);
+                  handleClickTable(5, human);
                   setChannel5(human);
                 }}
               >
@@ -128,7 +284,7 @@ function App() {
                 my="auto"
                 className="box"
                 onClick={() => {
-                  handleClickTable(6);
+                  handleClickTable(6, human);
                   setChannel6(human);
                 }}
               >
@@ -138,7 +294,7 @@ function App() {
                 my="auto"
                 className="box"
                 onClick={() => {
-                  handleClickTable(7);
+                  handleClickTable(7, human);
                   setChannel7(human);
                 }}
               >
@@ -148,7 +304,7 @@ function App() {
                 my="auto"
                 className="box"
                 onClick={() => {
-                  handleClickTable(8);
+                  handleClickTable(8, human);
                   setChannel8(human);
                 }}
               >
